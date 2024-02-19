14fa0b6ce1bb91badec060acc03990789643246b
v-Kaefer <vitor.guttler@edu.pucrs.br>
2024-02-18 22:11:06 -0300

    theradixhub repo clone
    - Clone from theradixhub.github.io

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+{/*import { useState } from 'react'*/}
+import './App.css'
+import { Button } from './components/ui/button.tsx'
+
+
+function App() {
+
+  return (
+    <div className='flex flex-col h-screen w-screen'> {/*+flex*/}
+      <section className='flex flex-col flex-grow justify-between bg-slate-800 '>
+        <header className='flex flex-col w-full min-h-10 max-h-20 shadow-lg shadow-slate-700'>
+          <div className='flex w-auto items-center justify-between'>
+            <div className='flex text-center items-center'>
+              <img src="/XRB.png" className=" m-3 rounded-3xl border-4 max-w-8 max-h-8" />
+              <div className='text-white text-lg font-semibold border-4 border-transparent'>The Radix Hub</div>
+            </div>
+          
+            {/*Headers list*/}
+            
+            <div className='w-auto items-end'><Button className='m-3'>Launch dApp</Button></div>
+          </div>
+        </header>
+
+        <div title="Content" className='flex bg-slate-700 w-full flex-grow'>
+          <div className='w-full h-full overflow-hidden'>
+            <div className='flex px-20 justify-between items-center w-full h-full '>
+              <div className='text-transparent'>a</div>
+              <div className='flex flex-col items-center'>
+                <h1 className=''>The Radix Hub</h1>
+                <div><a href='https://github.com/TheRadixHub' className=' text-3xl'>Under development</a></div>
+              </div>
+              <div className=' text-transparent'>a</div>
+            </div>
+          </div>
+        </div>
+
+        <footer className='flex flex-col justify-between items-center text-center bg-slate-900'>
+          <div className="flex w-full h-fit">
+            <div className='flex justify-between px-12 w-full'>
+              <div className='flex flex-row m-3 bg-transparent text-transparent max-h-10 items-center'>
+                
+                <a title='Discord' href="" className='mx-2'><img src='/discord_icon_white.svg' className='w-8 h-8'></img></a>
+                <a title='X' href="https://twitter.com/TheRadixHub" className='mx-2'><img src='/twitter_x_icon_white.svg' className='w-8 h-8'></img></a>
+                <a title="Whitepaper" href="https://bridge-route-x.gitbook.io/project-overview/" className='mx-2'><img alt="Whitepaper" src='/gitbook_white.svg' loading="lazy" className='w-8 h-8'></img></a>
+                <a title="Github" href='https://www.github.com/TheRadixHub' className='mx-2'><img src='/github_icon_white.svg' loading="lazy" className='w-8 h-8'></img></a>
+              </div>
+              <div>
+                <div className='flex flex-row text-white items-center'>
+                  <div title="Feedback link" className='flex flex-row w-fit m-3 text-center text-transparent'>
+                    <div className='text-transparent'>a</div>
+                    <div className='text-transparent'>Feedback</div>
+                  </div>
+                  <div className=''>©2024 The Radix Hub</div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </footer>
+      </section>
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+/// <reference types="vitest" />
+import path from "path"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/",
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  test: {
+    css: false,
+    
+  },
+})
